94374996c3a2b2ffdcf87b35af2683e3f4157bc1
Shree Bhagwat <shreebhagwat94@gmail.com>
2022-06-10 18:45:29 +0530

    added chat screen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class ChatScreen extends StatelessWidget {
-  const ChatScreen({super.key});
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key, required this.userUid, required this.userName});
+
+  final String userUid;
+  final String userName;
 
   @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.userName),
+      ),
+    );
   }
 }
--- lib/screens/users_screen.dart
+++ lib/screens/users_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/screens/chat_screen.dart';
 import 'package:chat_app/screens/login_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -36,47 +37,57 @@ class _UsersScreenState extends State<UsersScreen> {
         body: ListView.builder(
           itemCount: users.length,
           itemBuilder: (BuildContext context, int index) {
-            return Card(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding:
-                              const EdgeInsets.only(top: 20.0, bottom: 20.0),
-                          child: Text(
-                              '${users[index]['firstName']} ${users[index]['lastName']}'),
-                        ),
-                        Text(
-                          users[index]['age'].toString(),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(20.0),
-                          child: Container(
-                            width: 10,
-                            height: 10,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(5),
-                                color: users[index]['isOnline']
-                                    ? Colors.green
-                                    : Colors.red),
+            return GestureDetector(
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(top: 20.0, bottom: 20.0),
+                            child: Text(
+                                '${users[index]['firstName']} ${users[index]['lastName']}'),
                           ),
-                        ),
-                        Text(users[index]['timestamp']),
-                      ],
-                    ),
-                  ],
+                          Text(
+                            users[index]['age'].toString(),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Container(
+                              width: 10,
+                              height: 10,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  color: users[index]['isOnline']
+                                      ? Colors.green
+                                      : Colors.red),
+                            ),
+                          ),
+                          Text(users[index]['timestamp']),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (_) => ChatScreen(
+                            userUid: users[index]['uid'],
+                            userName: users[index]['firstName'])));
+              },
             );
           },
         ));
