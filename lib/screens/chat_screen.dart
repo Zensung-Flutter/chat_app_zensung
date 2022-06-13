28346d842d38db41387da02ac4bc5eda27ad7740
Shree Bhagwat <shreebhagwat94@gmail.com>
2022-06-13 21:59:09 +0530

    refactored code

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key, required this.userUid, required this.userName});
@@ -25,7 +24,6 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   void initState() {
     super.initState();
-
     getMessage();
   }
 
